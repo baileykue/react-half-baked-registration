5734858f2eef0b6adeaa75e602de80994e7e4667
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-10 12:49:59 -0800

    wrote out sign in page, similar to sign up

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { signInUser } from '../services/users';
+import AuthForm from '../components/AuthForm';
+import { useState } from 'react';
 
-export default function SignIn() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function SignUp() {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInUser(email, password);
+    } catch {
+      setErrorMessage(
+        'Oops, something went wrong, please make sure your email and password are correct'
+      );
+    }
+  };
+
+  return (
+    <div>
+      <AuthForm
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+        errorMessage={errorMessage}
+      />
+    </div>
+  );
 }
