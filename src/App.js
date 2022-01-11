a4d566b871d78bd833f1768ba2137b13b8b843fd
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-10 16:13:21 -0800

    getting fancy with some gradient backgrounds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
-import Auth from './views/Auth';
-import SignIn from './views/SignIn';
 import { getUser, signOut } from './services/users';
-import SignUp from './views/SignUp';
+import Auth from './views/Auth';
+import UserView from './components/UserView';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(getUser());
@@ -18,12 +17,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            {currentUser && (
-              <>
-                <h1>I am Signed In</h1>
-                <button onClick={signOutUser}>Log Out</button>
-              </>
-            )}
+            {currentUser && <UserView signOutUser={signOutUser} />}
             {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
           </Route>
         </Switch>
---
+++ src/components/UserView.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function UserView({ signOutUser }) {
+  return (
+    <div>
+      <header>
+        <button onClick={signOutUser}>Log Out</button>
+      </header>
+      <h2>Welcome User! We are happy you are here.</h2>
+      <iframe
+        src="https://giphy.com/embed/ONuQzM11fjvoY"
+        width="480"
+        height="367"
+        frameBorder="0"
+        className="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/gifs/cute-happy-ONuQzM11fjvoY">via GIPHY</a>
+      </p>
+    </div>
+  );
+}
